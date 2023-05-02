a5e62be364d08c6e4c24abd7a202b343245988b3
boris-96@hotmail.com <boris-96@hotmail.com>
2023-05-02 22:00:57 +0200

    Assignment 3

--- src/com/jwetherell/algorithms/sorts/BubbleSort.java
+++ src/com/jwetherell/algorithms/sorts/BubbleSort.java
@@ -1,5 +1,7 @@
 package com.jwetherell.algorithms.sorts;
 
+import org.checkerframework.checker.index.qual.IndexFor;
+
 /**
  * Bubble sort is a simple sorting algorithm that works by repeatedly stepping
  * through the list to be sorted, comparing each pair of adjacent items and
@@ -14,34 +16,40 @@ package com.jwetherell.algorithms.sorts;
  * Worst case = O(n^2)<br>
  * Best case = O(n)<br>
  * <p>
- * @see <a href="https://en.wikipedia.org/wiki/Bubble_sort">Bubble Sort (Wikipedia)</a>
- * <br>
+ * 
+ * @see <a href="https://en.wikipedia.org/wiki/Bubble_sort">Bubble Sort
+ *      (Wikipedia)</a>
+ *      <br>
  * @author Justin Wetherell <phishman3579@gmail.com>
  */
 public class BubbleSort<T extends Comparable<T>> {
 
-    private BubbleSort() { }
-    //@param unsorted array
-    //@return sorted array
-    public static <T extends Comparable<T>> T[] sort(T[] unsorted) {
-        boolean swapped = true;
-        int length = unsorted.length;
-        while (swapped) {
-            swapped = false;
-            for (int i = 1; i < length; i++) {
-                if (unsorted[i].compareTo(unsorted[i - 1]) < 0) {
-                    swap(i, i - 1, unsorted);
-                    swapped = true;
-                }
-            }
-            length--;
-        }
-        return unsorted;
-    }
-    //swapping the value
-    private static <T extends Comparable<T>> void swap(int index1, int index2, T[] unsorted) {
-        T value = unsorted[index1];
-        unsorted[index1] = unsorted[index2];
-        unsorted[index2] = value;
-    }
+	private BubbleSort() {
+	}
+
+	// @param unsorted array
+	// @return sorted array
+	public static <T extends Comparable<T>> T[] sort(T[] unsorted) {
+		boolean swapped = true;
+		int length = unsorted.length;
+		while (swapped) {
+			swapped = false;
+			for (int i = 1; i < length; i++) {
+				if (unsorted[i].compareTo(unsorted[i - 1]) < 0) {
+					swap(i, i - 1, unsorted);
+					swapped = true;
+				}
+			}
+			length--;
+		}
+		return unsorted;
+	}
+
+	// swapping the value
+	private static <T extends Comparable<T>> void swap(@IndexFor("#3") int index1, @IndexFor("#3") int index2,
+			T[] unsorted) {
+		T value = unsorted[index1];
+		unsorted[index1] = unsorted[index2];
+		unsorted[index2] = value;
+	}
 }
--- src/com/jwetherell/algorithms/sorts/QuickSort.java
+++ src/com/jwetherell/algorithms/sorts/QuickSort.java
@@ -2,12 +2,18 @@ package com.jwetherell.algorithms.sorts;
 
 import java.util.Random;
 
+import org.checkerframework.checker.index.qual.IndexFor;
+import org.checkerframework.common.value.qual.MinLen;
+import org.checkerframework.checker.index.qual.NonNegative;
+
 /**
- * Quicksort is a sorting algorithm which, on average, makes O(n*log n) comparisons to sort
- * n items. In the worst case, it makes O(n^2) comparisons, though this behavior is
- * rare. Quicksort is often faster in practice than other algorithms. 
+ * Quicksort is a sorting algorithm which, on average, makes O(n*log n)
+ * comparisons to sort
+ * n items. In the worst case, it makes O(n^2) comparisons, though this behavior
+ * is
+ * rare. Quicksort is often faster in practice than other algorithms.
  * <p>
- * Family: Divide and conquer.<br> 
+ * Family: Divide and conquer.<br>
  * Space: In-place.<br>
  * Stable: False.<br>
  * <p>
@@ -15,70 +21,85 @@ import java.util.Random;
  * Worst case = O(n^2)<br>
  * Best case = O(n) [three-way partition and equal keys]<br>
  * <p>
- * @see <a href="https://en.wikipedia.org/wiki/Quick_sort">Quicksort (Wikipedia)</a>
- * <br>
+ * 
+ * @see <a href="https://en.wikipedia.org/wiki/Quick_sort">Quicksort
+ *      (Wikipedia)</a>
+ *      <br>
  * @author Justin Wetherell <phishman3579@gmail.com>
  */
 public class QuickSort<T extends Comparable<T>> {
 
-    private static final Random RAND = new Random();
+	private static final Random RAND = new Random();
 
-    public static enum PIVOT_TYPE {
-        FIRST, MIDDLE, RANDOM
-    }
+	public static enum PIVOT_TYPE {
+		FIRST, MIDDLE, RANDOM
+	}
 
-    public static PIVOT_TYPE type = PIVOT_TYPE.RANDOM;
+	public static PIVOT_TYPE type = PIVOT_TYPE.RANDOM;
 
-    private QuickSort() { }
+	private QuickSort() {
+	}
 
-    public static <T extends Comparable<T>> T[] sort(PIVOT_TYPE pivotType, T[] unsorted) {
-        int pivot = 0;
-        if (pivotType == PIVOT_TYPE.MIDDLE) {
-            pivot = unsorted.length/2;
-        } else if (pivotType == PIVOT_TYPE.RANDOM) {
-            pivot = getRandom(unsorted.length);  
-        }
-        sort(pivot, 0, unsorted.length - 1, unsorted);
-        return unsorted;
-    }
+	@SuppressWarnings({ "assignment" })
+	public static <T extends Comparable<T>> T[] sort(PIVOT_TYPE pivotType, T @MinLen(1) [] unsorted) {
 
-    private static <T extends Comparable<T>> void sort(int index, int start, int finish, T[] unsorted) {
-        int pivotIndex = start + index;
-        T pivot = unsorted[pivotIndex];
-        int s = start;
-        int f = finish;
-        while (s <= f) {
-            while (unsorted[s].compareTo(pivot) < 0)
-                s++;
-            while (unsorted[f].compareTo(pivot) > 0)
-                f--;
-            if (s <= f) {
-                swap(s, f, unsorted);
-                s++;
-                f--;
-            }
-        }
-        if (start < f) {
-            pivotIndex = getRandom((f - start) + 1);
-            sort(pivotIndex, start, f, unsorted);
-        }
-        if (s < finish) {
-            pivotIndex = getRandom((finish - s) + 1);
-            sort(pivotIndex, s, finish, unsorted);
-        }
-    }
+		@IndexFor("unsorted")
+		int pivot = 0;
+		if (pivotType == PIVOT_TYPE.MIDDLE) {
+			pivot = unsorted.length / 2;
+		} else if (pivotType == PIVOT_TYPE.RANDOM) {
+			pivot = getRandom(unsorted.length);
+		}
+		sort(pivot, 0, unsorted.length - 1, unsorted);
+		return unsorted;
+	}
 
-    private static final int getRandom(int length) {
-        if (type == PIVOT_TYPE.RANDOM && length > 0)
-            return RAND.nextInt(length);
-        if (type == PIVOT_TYPE.FIRST && length > 0)
-            return 0;
-        return length / 2;
-    }
+	@SuppressWarnings({ "unary.increment", "unary.decrement", "assignment" })
+	private static <T extends Comparable<T>> void sort(
+			@IndexFor("#4") int index,
+			@IndexFor("#4") int start,
+			@IndexFor("#4") int finish,
+			T[] unsorted) {
+		@IndexFor("unsorted")
+		int pivotIndex = start + index;
+		T pivot = unsorted[pivotIndex];
+		@IndexFor("unsorted")
+		int s = start;
+		@IndexFor("unsorted")
+		int f = finish;
+		while (s <= f) {
+			while (unsorted[s].compareTo(pivot) < 0)
+				s++;
+			while (unsorted[f].compareTo(pivot) > 0)
+				f--;
+			if (s <= f) {
+				swap(s, f, unsorted);
+				s++;
+				f--;
+			}
+		}
+		if (start < f) {
+			pivotIndex = getRandom((f - start) + 1);
+			sort(pivotIndex, start, f, unsorted);
+		}
+		if (s < finish) {
+			pivotIndex = getRandom((finish - s) + 1);
+			sort(pivotIndex, s, finish, unsorted);
+		}
+	}
 
-    private static <T extends Comparable<T>> void swap(int index1, int index2, T[] unsorted) {
-        T index2Element = unsorted[index1];
-        unsorted[index1] = unsorted[index2];
-        unsorted[index2] = index2Element;
-    }
+	private static final @NonNegative int getRandom(@NonNegative int length) {
+		if (type == PIVOT_TYPE.RANDOM && length > 0)
+			return RAND.nextInt(length);
+		if (type == PIVOT_TYPE.FIRST && length > 0)
+			return 0;
+		return length / 2;
+	}
+
+	private static <T extends Comparable<T>> void swap(@IndexFor("#3") int index1, @IndexFor("#3") int index2,
+			T[] unsorted) {
+		T index2Element = unsorted[index1];
+		unsorted[index1] = unsorted[index2];
+		unsorted[index2] = index2Element;
+	}
 }
