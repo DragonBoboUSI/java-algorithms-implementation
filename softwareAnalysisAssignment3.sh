75bb0bb5c466956aba2055ee96dfb0a7bf893354
boris-96@hotmail.com <boris-96@hotmail.com>
2023-05-02 22:05:22 +0200

    Added script

---
+++ softwareAnalysisAssignment3.sh
@@ -0,0 +1,4 @@
+javacheck -processor org.checkerframework.checker.index.IndexChecker src/com/jwetherell/algorithms/sorts/BubbleSort.java
+javacheck -processor org.checkerframework.checker.index.IndexChecker src/com/jwetherell/algorithms/sorts/InsertionSort.java
+javacheck -processor org.checkerframework.checker.index.IndexChecker src/com/jwetherell/algorithms/sorts/QuickSort.java
+javacheck -processor org.checkerframework.checker.index.IndexChecker src/com/jwetherell/algorithms/sorts/ShellSort.java
